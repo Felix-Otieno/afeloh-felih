bc84933ab11264d2351d21fcb933b4bf01e6b38b
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-09 10:46:56 +0300

    add changes of score

---
+++ score.js
@@ -0,0 +1,22 @@
+let score = -180;
+
+if (score > 100 || score < 0) {
+    console.log("display an error");
+} else if (score >= 90) {
+    console.log("You got an A");
+} else if (score >= 80) {
+    console.log("You got a B");
+} else if (score >= 70) {
+    console.log("You got a C");
+} else if (score >= 60) {
+    console.log("You got an D");
+} else if (score >= 50) {
+    console.log("You got an E");
+} else {
+    console.log("You got a F");
+}
+
+
+
+
+
